95f474da2656ae80916121f5f415c1dafc9949b5
Ali Craigmile <ali.craigmile@bbc.co.uk>
2016-10-30 22:35:14 +0000

    stub tests for disabling fields during refresh

---
+++ test/browse.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var request = require('supertest'),
+    app = require('../lib/app'),
+		chai = require('chai'),
+		expect = chai.expect;
+
+describe('Browse', function() {
+    it('disable fields during refresh');
+});
