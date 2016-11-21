4ce150410ae9c869184a62d36b133144c0e4dfa6
Ali Craigmile <ali.craigmile@bbc.co.uk>
2016-11-21 21:53:10 +0000

    chore(tests for feedback)

---
+++ test/feedback.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var request = require('supertest'),
+    app = require('../lib/app'),
+		chai = require('chai'),
+		expect = chai.expect;
+
+describe('Feedback', function() {
+    it('allows me to report a bug');
+    it('allows me to suggest a feature');
+});
